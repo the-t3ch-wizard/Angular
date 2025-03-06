e2e0f0d1086a26c729918571a7824303f3517e44
ayush <ayushmaurya2050@gmail.com>
2025-03-06 16:24:42 +0530

    [Ayush kumar maurya] Add: onClick handler using Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Angular';
   imageUrl = "logo.jpg";
+  onClickHref = "https://www.google.com"
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
+  }
+
+  logoOnClickHandler($event: Event){
+    // console.log("clicked", $event);
+    window.open(this.onClickHref, "_blank")
   }
 }
